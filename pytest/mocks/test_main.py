b5c46504fa52edb82bd57a6ee71c24381b6fda0c
RohilPrajapati <rohilprajapati@gmail.com>
2025-09-05 21:05:33 +0545

    fixed: typo pytest basic test

---
+++ pytest/mocks/test_main.py
@@ -0,0 +1,18 @@
+import pytest
+from main import get_weather
+
+
+def test_get_weather(mocker):
+    mock_get = mocker.patch("main.requests.get")
+
+    # set return values
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = {"temperature": 25, "condition": "Sunny"}
+
+    # call function
+    result = get_weather("Dubai")
+
+    # assertions
+    assert result == {"temperature": 25, "condition": "Sunny"}
+    mock_get.assert_called_once_with("https://api.weather.com/v1/Dubai")
+ 
