567ec6c990969c44e07529da8110181b54656e2d
RohilPrajapati <rohilprajapati@gmail.com>
2026-01-13 21:35:03 +0545

    added dataclass type hint

---
+++ 2026/mypy/using_data_class.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from typing import NewType
+
+RGB = NewType("RGB", tuple[int, int, int])
+HSL = NewType("HSL", tuple[int, int, int])
+
+
+@dataclass
+class User:
+    first_name: str
+    last_name: str
+    email: str
+    age: int | None = None
+    fav_color: RGB | None = None
+
+
+def create_user(
+    first_name: str,
+    last_name: str,
+    age: int | None = None,
+    fav_color: RGB | None = None,
+) -> User:
+    email = f"{first_name.lower()}_{last_name.lower()}@example.com"
+
+    return User(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        age=age,
+        fav_color=fav_color,
+    )
+
+
+user1 = create_user("Rohil", "Prajapati", age=25, fav_color=RGB((129, 133, 214)))
+user2 = create_user("John", "Doe", fav_color=RGB((206, 10, 65)))
+
+print(user1)
+print(user2)
